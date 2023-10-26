ef26684d4a7dcd79d531677aee4fc7bbdf9251f2
Tyra Jamison <53059879+mantiscodes@users.noreply.github.com>
2023-10-26 02:22:55 -0400

    Add files via upload
    This is not finished. I'd like to speak with you all personally about how this work load is impacting me and I would also like to have a conversation with the cohort about how this workload is impacting them and figure out some solutions.

---
+++ script.js
@@ -0,0 +1,180 @@
+// Javascript Total Recall 
+//=================================VARIABLES & DATATYPES============================================//
+//--------------------A. Q&A
+
+// 1. How do we assign value to a new variable?
+ // First you would name the new variable, and use the equal sign "=" to assign the value. You can assign the value as a string, a character, or a number.
+
+//2. How do we change the value of a variable?
+//You can either change what's placed on the other side of the equal sign or you can declare it as a new variable. 
+
+//3. Remind me, what are declare, assign, & define?
+//Declaring is naming the variable, assigning is creating value for the variable, and defining is when you put attributes to the variable. 
+
+//4. What is pseudocoding and why should you do it?
+//Pseudocoding is when you write out the basic logic of code, a function, or a process that you want to incorporate. It is not bound to any programming language or syntax, you are naming exactly what you want to do. It's important for mapping out how you want a function or a piece of code to move. 
+//5. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+//It's important to be active in seeking your solutions. You never know unless you try. 
+
+//--------------------B. Strings 
+
+// let firstVariable = 'Hello World';
+
+// firstVariable = 5;
+
+// let secondVariable = firstVariable;
+
+// secondVariable = "I get it now";
+
+// console.log(firstVariable)
+
+// //firstVariable value = 5
+
+// let yourName = "Tyra";
+
+
+
+//--------------------C. Booleans
+
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
+// const e = 'Kevin';
+
+// console.log(a < b);
+// console.log(c > d);
+// console.log('Oya' == 'Yansa');
+// // FOR THE NEXT TWO, USE ONLY && OR ||
+// console.log(true || false);
+// console.log(false && false && false && false && false || true);
+// console.log(false == false)
+// console.log(e = 'Kevin');
+// console.log(a + b == c);
+// // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+// console.log(a * a == d); 
+// // note: the answer is a simple arithmetic equation, not something "weird"
+// console.log(48 == '48');
+
+
+//--------------------D. The farm
+
+// let animal = "cow";
+// if(animal = "cow"){ console.log("moooo!")} else
+//     {console.log("Hey! You're not a cow!")
+// };
+
+
+// // --------------------E. Driver's Ed
+
+// let yourAge = 13;
+// if (yourAge >= 16) {console.log("Here are the keys!")} else{
+//     console.log("Sorry, you're too young.")
+// };
+
+//=========================================LOOPS================================================//
+
+//---------------------A. The Basics
+
+//1.  for (let i = 1; i<=10;i++){
+//     console.log(i);
+// } 
+
+//2. 
+//  for (let i = 10; i<=400;i++){
+//     console.log(i);
+//  }
+//3. 
+
+// let i = i + 3;
+// for (let i = 12; i<=4000;i++){
+//     console.log(n);
+// }
+//---------------------B. Get Even
+// for (let i=1; i<=100;i++)
+// {console.log(i)};
+//     if(i === 0)
+//     {console.log(i + 'is even')};
+//     else(i % 2 === 0)
+//     {console.log(i + 'is even')};
+
+
+//---------------------C. Give Me Five
+
+
+// for(let i=1; i<=100;i++){
+// if (i % 5 && i % 3){
+//     console.log("I found a" + i + "three is a crowd" + "high five!");
+//  } else {
+//     if(i % 5 == 0) {
+//         console.log("I found a" + i + "high five!");
+//     }
+//     else {
+//         if(i % 3 == 0){
+//             console.log("I found a" + i + "three is a crowd");
+//         }
+//     }
+// }
+// }
+//---------------------D. Savings Account
+// I honestly am stuck on this one...I am exhausted...and would like to work on this at a different time because I am trying to complete as much of this assignment as I can. 
+
+//===================================ARRAYS & CONTROL FLOW======================================//
+
+//----------------------A. Talk About It
+
+//1. What are the things an array is called?
+
+
+//2. Do Arrays gurantee those things will be in order?
+
+//3. What real life thing could you model with an array?
+
+//----------------------B. Easy Does It
+
+//----------------------C. Accessing Elements
+
+//----------------------D. Change Values
+
+//----------------------E. Mix It Up 
+
+//----------------------F. Biggie Smalls
+
+//----------------------G. Monkey in the Middle
+
+//----------------------H. What's in your Closet?
+
+//=================================FUNCTIONS======================================================//
+
+//--------------------------A. printGreeting
+
+//--------------------------B. printCool
+
+//--------------------------C. calculateCube
+
+//--------------------------D. isVowel
+
+//--------------------------E. getTwoLengths
+
+//--------------------------F. getMultipleLengths
+
+//--------------------------G. maxOfThree
+
+//--------------------------H.printLongestWord
+
+
+//=========================================OBJECTS=================================================//
+
+//--------------------------A. Make a user object 
+
+//--------------------------B. Update the user 
+
+//--------------------------C. Adding the keys and values 
+
+//--------------------------D. Shopaholic! 
+
+//--------------------------E. Object-within-object 
+
+//--------------------------F. Loops
+
+//--------------------------G. Functions can operate on objects
